c60916b39d889db2e5dca755199cecb99935f96f
Yunikabputri <114121837+Yunikabputri@users.noreply.github.com>
2023-07-25 12:24:53 +0700

    Update Anggota.java

--- PBO2_2101092025/src/yunika/model/Anggota.java
+++ PBO2_2101092025/src/yunika/model/Anggota.java
@@ -10,52 +10,55 @@ package yunika.model;
  * @author ynkax25
  */
 public class Anggota {
-    private String kodeAnggota;
-    private String namaAnggota;
-    private String alamat;
-    private String jenisKelamin;
+    private String KodeAnggota;
+    private String NamaAnggota;
+    private String Alamat;
+    private String JenisKelamin;
     
     public Anggota(){
+        
+    }
+
+    public Anggota(String KodeAnggota, String NamaAnggota, String Alamat, String JenisKelamin) {
+        this.KodeAnggota = KodeAnggota;
+        this.NamaAnggota = NamaAnggota;
+        this.Alamat = Alamat;
+        this.JenisKelamin = JenisKelamin;
+    }
     
-    }
-
-    public Anggota(String kodeAnggota, String namaAnggota, String alamat, String jenisKelamin) {
-        this.kodeAnggota = kodeAnggota;
-        this.namaAnggota = namaAnggota;
-        this.alamat = alamat;
-        this.jenisKelamin = jenisKelamin;
-    }
-
     
-    public String getKodeanggota() {
-        return kodeAnggota;
+
+    public String getKodeAnggota() {
+        return KodeAnggota;
     }
 
-    public void setKodeanggota(String kodeAnggota) {
-        this.kodeAnggota = kodeAnggota;
+    public void setKodeAnggota(String KodeAnggota) {
+        this.KodeAnggota = KodeAnggota;
     }
 
-    public String getNamaanggota() {
-        return namaAnggota;
+    public String getNamaAnggota() {
+        return NamaAnggota;
     }
 
-    public void setNamaanggota(String namaAnggota) {
-        this.namaAnggota = namaAnggota;
+    public void setNamaAnggota(String NamaAnggota) {
+        this.NamaAnggota = NamaAnggota;
     }
 
-    public String getalamat() {
-        return alamat;
+    public String getAlamat() {
+        return Alamat;
     }
 
-    public void setalamat(String alamat) {
-        this.alamat = alamat;
+    public void setAlamat(String Alamat) {
+        this.Alamat = Alamat;
     }
 
-    public String getjenisKelamin() {
-        return jenisKelamin;
+    public String getJenisKelamin() {
+        return JenisKelamin;
     }
 
-    public void setjenisKelamin(String jenisKelamin) {
-        this.jenisKelamin = jenisKelamin;
+    public void setJenisKelamin(String JenisKelamin) {
+        this.JenisKelamin = JenisKelamin;
     }
+    
+    
 }
